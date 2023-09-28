4c6d7b5099af2579b7123446c0db43b37fc9fdc3
Gabriel Silva <140929886+Gabriel-D-EV@users.noreply.github.com>
2023-09-28 18:00:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import Tk, StringVar
 from tkinter import ttk
 from PIL import Image, ImageTk
+from tkcalendar import Calendar, DateEntry
+from datetime import date
 
 
 
@@ -39,6 +41,7 @@ framem.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 framef = Frame(janela, width=1045, height=300, bg=co1, relief=FLAT)
 framef.grid(row=2, column=0, pady=0, padx=1, sticky=NSEW)
 
+#FRAMEH
 #imagem]
 app_img = Image.open('imagens/list.png')
 app_img = app_img.resize((45,45))
@@ -46,6 +49,53 @@ app_img = ImageTk.PhotoImage(app_img)
 
 logo = Label(frameh,image=app_img, text=' Inventário!', width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Arial 20 bold'), bg=co4, fg=co3)
 logo.place(x=0,y=0)
+
+#FRAMEM
+#inputs
+#nome
+lnome = Label(framem, text='Nome', height=1, anchor=NW, font=('Arial 10 bold'), bg=co1, fg=co3)
+lnome.place(x=12,y=10)
+
+enome = Entry(framem, width=40, justify='left', relief=SOLID)
+enome.place(x=100, y=12)
+
+#descrição
+ldesc = Label(framem, text='Descrição', height=1, anchor=NW, font=('Arial 10 bold'), bg=co1, fg=co3)
+ldesc.place(x=12,y=40)
+
+edesc = Entry(framem, width=40, justify='left', relief=SOLID)
+edesc.place(x=100, y=42)
+
+#marca
+lmarca = Label(framem, text='Marca', height=1, anchor=NW, font=('Arial 10 bold'), bg=co1, fg=co3)
+lmarca.place(x=12,y=70)
+
+emarca = Entry(framem, width=40, justify='left', relief=SOLID)
+emarca.place(x=100, y=72)
+
+#data
+ldata = Label(framem, text='Data', height=1, anchor=NW, font=('Arial 10 bold'), bg=co1, fg=co3)
+ldata.place(x=12,y=100)
+
+edata = DateEntry(framem, width=15, Background=co1, bordewidth=2, year=2023)
+edata.place(x=100, y=102)
+
+#valor
+lvalor = Label(framem, text='Valor', height=1, anchor=NW, font=('Arial 10 bold'), bg=co1, fg=co3)
+lvalor.place(x=12,y=130)
+
+evalor = Entry(framem, width=40, justify='left', relief=SOLID)
+evalor.place(x=100, y=132)
+
+'''#imagem
+lnome = Label(framem, text='Nome', height=1, anchor=NW, font=('Arial 10 bold'), bg=co4, fg=co3)
+lnome.place(x=12,y=10)
+
+enome = Entry(framem, width=40, justify='left', relief=SOLID)
+enome.place(x=60, y=12)'''
+
+
+
 
 janela.mainloop()
 
